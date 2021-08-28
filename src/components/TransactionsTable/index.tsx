2217cc70858a78e0b97c9858e3918d0b10510a58
Cleberton Junior <cleberton.juninho@gmail.com>
2021-08-28 19:33:52 -0300

    first commit

---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,32 @@
+import { Container } from "./styles";
+
+export function TransactionsTable() {
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Títolo</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Desenvolvimento de Web Site</td>
+            <td className="deposit">R$5.000</td>
+            <td>Desenvolvimento</td>
+            <td>20/06/2021</td>
+          </tr>
+          <tr>
+            <td>Aluguel</td>
+            <td className="withdraw">- R$1.000</td>
+            <td>Casa</td>
+            <td>01/08/2021</td>
+          </tr>
+        </tbody>
+      </table>
+    </Container>
+  );
+}
